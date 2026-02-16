86b76f883a109212d4db5fc789fa14e99d99d0f1
Edward <delaport@illinois.edu>
2026-02-16 11:17:24 -0600

    Fix for --make-target

--- src/borg/borg.py
+++ src/borg/borg.py
@@ -127,14 +127,6 @@ def init_parser():
         "--debug",
         action=argparse.BooleanOptionalAction,
         help="Print verbose debug output")
-    parser.add_argument(
-        "-m",
-        "--make-target",
-        metavar="TARGET",
-        type=MakeDependencyFile('w'),
-        help="Writes a Makefile dependency TARGET.d file: "
-        "TARGET.d will configure the TARGET to depend on "
-        "the files checked by borg compare.")
     subparsers = parser.add_subparsers()
 
     update = subparsers.add_parser('update', aliases=['up'])
@@ -142,6 +134,15 @@ def init_parser():
 
     compare = subparsers.add_parser('compare', aliases=['cmp'])
     compare.set_defaults(func=compare_repo)
+    compare.add_argument(
+        "-m",
+        "--make-target",
+        metavar="TARGET",
+        type=MakeDependencyFile('w'),
+        help="Writes a Makefile dependency TARGET.d file: "
+        "TARGET.d will configure the TARGET to depend on "
+        "the files checked by borg compare.")
+
 
     gen = subparsers.add_parser('generate', aliases=['gen'])
     gen.add_argument('FILE', choices=('.gitattributes', ),
